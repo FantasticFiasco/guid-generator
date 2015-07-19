ed53e449f791363f9ce69952e7fe412a91d83c20
FantasticFiasco <mattias.kindborg@gmail.com>
2015-07-19 23:32:16 +0200

    Implemented the different GUID formats provided by 'guidgen.exe', making this tool a complete replacement instead of a complement.

--- src/ShortGuidGenerator/MainForm.Designer.cs
+++ src/ShortGuidGenerator/MainForm.Designer.cs
@@ -35,7 +35,17 @@
             this.groupBoxResult = new System.Windows.Forms.GroupBox();
             this.labelResult = new System.Windows.Forms.Label();
             this.labelDescription = new System.Windows.Forms.Label();
+            this.groupBoxGuidFormat = new System.Windows.Forms.GroupBox();
+            this.radioButtonShortFormat = new System.Windows.Forms.RadioButton();
+            this.radioButtonTagFormat = new System.Windows.Forms.RadioButton();
+            this.radioButtonAttributeFormat = new System.Windows.Forms.RadioButton();
+            this.radioButtonRegistryFormat = new System.Windows.Forms.RadioButton();
+            this.radioButtonConstFormat = new System.Windows.Forms.RadioButton();
+            this.radioButtonDefineFormat = new System.Windows.Forms.RadioButton();
+            this.radioButtonOleCreateFormat = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBoxResult.SuspendLayout();
+            this.groupBoxGuidFormat.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonCopy
@@ -45,7 +55,7 @@
             this.buttonCopy.Name = "buttonCopy";
             this.buttonCopy.Size = new System.Drawing.Size(75, 23);
             this.buttonCopy.TabIndex = 0;
-            this.buttonCopy.Text = "Copy";
+            this.buttonCopy.Text = "&Copy";
             this.buttonCopy.UseVisualStyleBackColor = true;
             this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
             // 
@@ -56,7 +66,7 @@
             this.buttonNewGuid.Name = "buttonNewGuid";
             this.buttonNewGuid.Size = new System.Drawing.Size(75, 23);
             this.buttonNewGuid.TabIndex = 1;
-            this.buttonNewGuid.Text = "New GUID";
+            this.buttonNewGuid.Text = "&New GUID";
             this.buttonNewGuid.UseVisualStyleBackColor = true;
             this.buttonNewGuid.Click += new System.EventHandler(this.buttonNewGuid_Click);
             // 
@@ -68,7 +78,7 @@
             this.buttonExit.Name = "buttonExit";
             this.buttonExit.Size = new System.Drawing.Size(75, 23);
             this.buttonExit.TabIndex = 2;
-            this.buttonExit.Text = "Exit";
+            this.buttonExit.Text = "E&xit";
             this.buttonExit.UseVisualStyleBackColor = true;
             this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
             // 
@@ -78,39 +88,150 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBoxResult.Controls.Add(this.labelResult);
-            this.groupBoxResult.Location = new System.Drawing.Point(12, 70);
+            this.groupBoxResult.Location = new System.Drawing.Point(12, 282);
             this.groupBoxResult.Name = "groupBoxResult";
-            this.groupBoxResult.Size = new System.Drawing.Size(264, 73);
+            this.groupBoxResult.Size = new System.Drawing.Size(345, 88);
             this.groupBoxResult.TabIndex = 1;
             this.groupBoxResult.TabStop = false;
             this.groupBoxResult.Text = "Result";
             // 
             // labelResult
             // 
-            this.labelResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.labelResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelResult.Location = new System.Drawing.Point(6, 20);
+            this.labelResult.Location = new System.Drawing.Point(6, 19);
+            this.labelResult.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
             this.labelResult.Name = "labelResult";
-            this.labelResult.Size = new System.Drawing.Size(252, 13);
+            this.labelResult.Size = new System.Drawing.Size(333, 66);
             this.labelResult.TabIndex = 0;
             // 
             // labelDescription
             // 
             this.labelDescription.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.labelDescription.Location = new System.Drawing.Point(12, 12);
+            this.labelDescription.Location = new System.Drawing.Point(12, 9);
             this.labelDescription.Name = "labelDescription";
-            this.labelDescription.Size = new System.Drawing.Size(264, 81);
+            this.labelDescription.Size = new System.Drawing.Size(264, 84);
             this.labelDescription.TabIndex = 2;
-            this.labelDescription.Text = "Select \"Copy\" to copy the result to the clipboard (the result can then be pasted " +
-    "into your source code). Choose \"Exit\" when done.";
+            this.labelDescription.Text = "Choose the desired format below, then select \"Copy\" to copy the result to the cli" +
+    "pboard (the result can then be pasted into your source code). Choose \"Exit\" when" +
+    " done.";
+            // 
+            // groupBoxGuidFormat
+            // 
+            this.groupBoxGuidFormat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxGuidFormat.Controls.Add(this.radioButtonShortFormat);
+            this.groupBoxGuidFormat.Controls.Add(this.radioButtonTagFormat);
+            this.groupBoxGuidFormat.Controls.Add(this.radioButtonAttributeFormat);
+            this.groupBoxGuidFormat.Controls.Add(this.radioButtonRegistryFormat);
+            this.groupBoxGuidFormat.Controls.Add(this.radioButtonConstFormat);
+            this.groupBoxGuidFormat.Controls.Add(this.radioButtonDefineFormat);
+            this.groupBoxGuidFormat.Controls.Add(this.radioButtonOleCreateFormat);
+            this.groupBoxGuidFormat.Controls.Add(this.label1);
+            this.groupBoxGuidFormat.Location = new System.Drawing.Point(12, 96);
+            this.groupBoxGuidFormat.Name = "groupBoxGuidFormat";
+            this.groupBoxGuidFormat.Size = new System.Drawing.Size(264, 180);
+            this.groupBoxGuidFormat.TabIndex = 3;
+            this.groupBoxGuidFormat.TabStop = false;
+            this.groupBoxGuidFormat.Text = "GUID Format";
+            // 
+            // radioButtonShortFormat
+            // 
+            this.radioButtonShortFormat.AutoSize = true;
+            this.radioButtonShortFormat.Location = new System.Drawing.Point(6, 157);
+            this.radioButtonShortFormat.Name = "radioButtonShortFormat";
+            this.radioButtonShortFormat.Size = new System.Drawing.Size(92, 17);
+            this.radioButtonShortFormat.TabIndex = 7;
+            this.radioButtonShortFormat.Text = "&7. Short GUID";
+            this.radioButtonShortFormat.UseVisualStyleBackColor = true;
+            this.radioButtonShortFormat.CheckedChanged += new System.EventHandler(this.radioButtonGuidFormat_CheckedChanged);
+            // 
+            // radioButtonTagFormat
+            // 
+            this.radioButtonTagFormat.AutoSize = true;
+            this.radioButtonTagFormat.Location = new System.Drawing.Point(6, 134);
+            this.radioButtonTagFormat.Name = "radioButtonTagFormat";
+            this.radioButtonTagFormat.Size = new System.Drawing.Size(185, 17);
+            this.radioButtonTagFormat.TabIndex = 6;
+            this.radioButtonTagFormat.Text = "&6. <Guid(\"xxxxxxxx-xxxx ... xxxx\")>";
+            this.radioButtonTagFormat.UseVisualStyleBackColor = true;
+            this.radioButtonTagFormat.CheckedChanged += new System.EventHandler(this.radioButtonGuidFormat_CheckedChanged);
+            // 
+            // radioButtonAttributeFormat
+            // 
+            this.radioButtonAttributeFormat.AutoSize = true;
+            this.radioButtonAttributeFormat.Location = new System.Drawing.Point(6, 111);
+            this.radioButtonAttributeFormat.Name = "radioButtonAttributeFormat";
+            this.radioButtonAttributeFormat.Size = new System.Drawing.Size(179, 17);
+            this.radioButtonAttributeFormat.TabIndex = 5;
+            this.radioButtonAttributeFormat.Text = "&5. [Guid(\"xxxxxxxx-xxxx ... xxxx\")]";
+            this.radioButtonAttributeFormat.UseVisualStyleBackColor = true;
+            this.radioButtonAttributeFormat.CheckedChanged += new System.EventHandler(this.radioButtonGuidFormat_CheckedChanged);
+            // 
+            // radioButtonRegistryFormat
+            // 
+            this.radioButtonRegistryFormat.AutoSize = true;
+            this.radioButtonRegistryFormat.Location = new System.Drawing.Point(6, 88);
+            this.radioButtonRegistryFormat.Name = "radioButtonRegistryFormat";
+            this.radioButtonRegistryFormat.Size = new System.Drawing.Size(245, 17);
+            this.radioButtonRegistryFormat.TabIndex = 4;
+            this.radioButtonRegistryFormat.Text = "&4. Registry Format (i.e. {xxxxxxxx-xxxx ... xxxx })";
+            this.radioButtonRegistryFormat.UseVisualStyleBackColor = true;
+            this.radioButtonRegistryFormat.CheckedChanged += new System.EventHandler(this.radioButtonGuidFormat_CheckedChanged);
+            // 
+            // radioButtonConstFormat
+            // 
+            this.radioButtonConstFormat.AutoSize = true;
+            this.radioButtonConstFormat.Location = new System.Drawing.Point(6, 65);
+            this.radioButtonConstFormat.Name = "radioButtonConstFormat";
+            this.radioButtonConstFormat.Size = new System.Drawing.Size(185, 17);
+            this.radioButtonConstFormat.TabIndex = 3;
+            this.radioButtonConstFormat.Text = "&3. static const struct GUID = { ... }";
+            this.radioButtonConstFormat.UseVisualStyleBackColor = true;
+            this.radioButtonConstFormat.CheckedChanged += new System.EventHandler(this.radioButtonGuidFormat_CheckedChanged);
+            // 
+            // radioButtonDefineFormat
+            // 
+            this.radioButtonDefineFormat.AutoSize = true;
+            this.radioButtonDefineFormat.Location = new System.Drawing.Point(6, 42);
+            this.radioButtonDefineFormat.Name = "radioButtonDefineFormat";
+            this.radioButtonDefineFormat.Size = new System.Drawing.Size(124, 17);
+            this.radioButtonDefineFormat.TabIndex = 2;
+            this.radioButtonDefineFormat.Text = "&2. DEFINE_GUID(...)";
+            this.radioButtonDefineFormat.UseVisualStyleBackColor = true;
+            this.radioButtonDefineFormat.CheckedChanged += new System.EventHandler(this.radioButtonGuidFormat_CheckedChanged);
+            // 
+            // radioButtonOleCreateFormat
+            // 
+            this.radioButtonOleCreateFormat.AutoSize = true;
+            this.radioButtonOleCreateFormat.Checked = true;
+            this.radioButtonOleCreateFormat.Location = new System.Drawing.Point(6, 19);
+            this.radioButtonOleCreateFormat.Name = "radioButtonOleCreateFormat";
+            this.radioButtonOleCreateFormat.Size = new System.Drawing.Size(188, 17);
+            this.radioButtonOleCreateFormat.TabIndex = 1;
+            this.radioButtonOleCreateFormat.TabStop = true;
+            this.radioButtonOleCreateFormat.Text = "&1. IMPLEMENT _OLECREATE(...)";
+            this.radioButtonOleCreateFormat.UseVisualStyleBackColor = true;
+            this.radioButtonOleCreateFormat.CheckedChanged += new System.EventHandler(this.radioButtonGuidFormat_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(6, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 13);
+            this.label1.TabIndex = 0;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonExit;
-            this.ClientSize = new System.Drawing.Size(369, 155);
+            this.ClientSize = new System.Drawing.Size(369, 382);
+            this.Controls.Add(this.groupBoxGuidFormat);
             this.Controls.Add(this.groupBoxResult);
             this.Controls.Add(this.buttonExit);
             this.Controls.Add(this.buttonNewGuid);
@@ -121,8 +242,10 @@
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Create Short GUID";
+            this.Text = "Create GUID";
             this.groupBoxResult.ResumeLayout(false);
+            this.groupBoxGuidFormat.ResumeLayout(false);
+            this.groupBoxGuidFormat.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -135,5 +258,14 @@
         private System.Windows.Forms.GroupBox groupBoxResult;
         private System.Windows.Forms.Label labelDescription;
         private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.GroupBox groupBoxGuidFormat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButtonOleCreateFormat;
+        private System.Windows.Forms.RadioButton radioButtonConstFormat;
+        private System.Windows.Forms.RadioButton radioButtonDefineFormat;
+        private System.Windows.Forms.RadioButton radioButtonRegistryFormat;
+        private System.Windows.Forms.RadioButton radioButtonAttributeFormat;
+        private System.Windows.Forms.RadioButton radioButtonTagFormat;
+        private System.Windows.Forms.RadioButton radioButtonShortFormat;
     }
 }
--- src/ShortGuidGenerator/MainForm.cs
+++ src/ShortGuidGenerator/MainForm.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Text;
 using System.Windows.Forms;
 
 namespace ShortGuidGenerator
 {
     public partial class MainForm : Form
     {
+        private Guid guid;
+
         public MainForm()
         {
             InitializeComponent();
@@ -12,9 +15,11 @@ namespace ShortGuidGenerator
             GenerateNewShortGuid();
         }
 
+        #region Event Handlers
+
         private void buttonCopy_Click(object sender, EventArgs e)
         {
-            Clipboard.SetText(labelResult.Text);
+            CopyToClipboard();
         }
 
         private void buttonNewGuid_Click(object sender, EventArgs e)
@@ -27,14 +32,148 @@ namespace ShortGuidGenerator
             Close();
         }
 
-        private void GenerateNewShortGuid()
+        private void radioButtonGuidFormat_CheckedChanged(object sender, EventArgs e)
         {
-            var guid = Guid.NewGuid();
-            string endodedGuid = Encode(guid);
-            labelResult.Text = endodedGuid;
+            if (((RadioButton)sender).Checked)
+            {
+                UpdateResult();
+            }
         }
 
-        public static string Encode(Guid guid)
+        #endregion
+
+        private void CopyToClipboard()
+        {
+            Clipboard.SetText(labelResult.Text);
+        }
+
+        private void GenerateNewShortGuid()
+        {
+            guid = Guid.NewGuid();
+            UpdateResult();
+        }
+
+        private void UpdateResult()
+        {
+            string result;
+
+            if (radioButtonOleCreateFormat.Checked)
+            {
+                result = EncodeOleCreate();
+            }
+            else if (radioButtonDefineFormat.Checked)
+            {
+                result = EncodeDefine();
+            }
+            else if (radioButtonConstFormat.Checked)
+            {
+                result = EncodeConst();
+            }
+            else if (radioButtonRegistryFormat.Checked)
+            {
+                result = RegistryEncode();
+            }
+            else if (radioButtonAttributeFormat.Checked)
+            {
+                result = AttributeEncode();
+            }
+            else if (radioButtonTagFormat.Checked)
+            {
+                result = TagEncode();
+            }
+            else if (radioButtonShortFormat.Checked)
+            {
+                result = ShortEncode();
+            }
+            else
+            {
+                throw new InvalidOperationException("Unsupported GUID format selected.");
+            }
+
+            labelResult.Text = result;
+        }
+
+        #region Encode
+
+        private string EncodeOleCreate()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendFormat("// {{{0}}}{1}", guid.ToString().ToUpper(), Environment.NewLine);
+            stringBuilder.AppendFormat("IMPLEMENT_OLECREATE(<<class>>, <<external_name>>, {0}", Environment.NewLine);
+            stringBuilder.AppendFormat("{0});", string.Join(", ", SplitHex()));
+            
+            return stringBuilder.ToString();
+        }
+
+        private string EncodeDefine()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendFormat("// {{{0}}}{1}", guid.ToString().ToUpper(), Environment.NewLine);
+            stringBuilder.AppendFormat("DEFINE_GUID(<<name>>, {0}", Environment.NewLine);
+            stringBuilder.AppendFormat("{0});", string.Join(", ", SplitHex()));
+
+            return stringBuilder.ToString();
+        }
+
+        private string EncodeConst()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendFormat("// {{{0}}}{1}", guid.ToString().ToUpper(), Environment.NewLine);
+            stringBuilder.AppendFormat("static const GUID <<name>> = {0}", Environment.NewLine);
+
+            string[] split = SplitHex();
+            stringBuilder.AppendFormat(
+                "{{ {0}, {1}, {2}, {{ {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10} }} }};",
+                split[0],
+                split[1],
+                split[2],
+                split[3],
+                split[4],
+                split[5],
+                split[6],
+                split[7],
+                split[8],
+                split[9],
+                split[10]);
+
+            return stringBuilder.ToString();
+        }
+
+        private string[] SplitHex()
+        {
+            string[] parts = guid.ToString().Split('-');
+            return new[]
+            {
+                string.Format("0x{0}", parts[0]),
+                string.Format("0x{0}", parts[1]),
+                string.Format("0x{0}", parts[2]),
+                string.Format("0x{0}", parts[3].Substring(0, 2)),
+                string.Format("0x{0}", parts[3].Substring(2, 2)),
+                string.Format("0x{0}", parts[4].Substring(0, 2)),
+                string.Format("0x{0}", parts[4].Substring(2, 2)),
+                string.Format("0x{0}", parts[4].Substring(4, 2)),
+                string.Format("0x{0}", parts[4].Substring(6, 2)),
+                string.Format("0x{0}", parts[4].Substring(8, 2)),
+                string.Format("0x{0}", parts[4].Substring(10, 2))
+            };
+        }
+
+        private string RegistryEncode()
+        {
+            return string.Format("{{{0}}}", guid.ToString().ToUpper());
+        }
+
+        private string AttributeEncode()
+        {
+            return string.Format("[Guid(\"{0}\")]", guid.ToString().ToUpper());
+        }
+
+        private string TagEncode()
+        {
+            return string.Format("<Guid(\"{0}\")>", guid.ToString().ToUpper());
+        }
+
+        private string ShortEncode()
         {
             // This code is copied from Mads Kristensen's blog at
             // http://madskristensen.net/post/a-shorter-and-url-friendly-guid
@@ -45,5 +184,7 @@ namespace ShortGuidGenerator
 
             return encodedGuid.Substring(0, 22);
         }
+
+        #endregion
     }
 }
